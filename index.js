4cbaff20bda288a434ab8abff8a62bf2de992089
Divyanshu Kumar <divyanshu10245@gmail.com>
2023-07-13 23:49:58 +0530

    modified some script and and added  html tags

--- index.js
+++ index.js
@@ -12,5 +12,54 @@ let openModal= function(){
 let closeModal= function(){
     console.log("close modal");
     modal.classList.remove("active");
-    overlay.classList.remove("overlayactive")
+    overlay.classList.remove("overlayactive");
 }
+
+const myInp=document.querySelector(".copy-link-input");
+const btnCopy = document.querySelector(".copy-link-button");
+
+// btnCopy.onclick=function(){
+
+//     myInp.select();
+ 
+//     document.execCommand("Copy");
+// }
+myInp.addEventListener("focus",function(){
+    myInp.select();
+})
+btnCopy.addEventListener("click",function(){
+    const text= myInp.value;
+    navigator.clipboard.writeText(text);
+    myInp.value = "Copied!";
+    setTimeout(() => myInp.value = text,1000);
+})
+// https://api.whatsapp.com/send?text=[post-title] [post-url]
+// https://www.facebook.com/sharer.php?u=[post-url]
+// https://twitter.com/share?url=[post-url]&text=[post-title]&via=[via]&hashtags=[hashtags]
+// https://www.linkedin.com/shareArticle?url=[post-url]&title=[post-title]
+// https://reddit.com/submit?url=[post-url]&title=[post-title]
+
+const fb=document.querySelector("#facebook")
+const tw=document.querySelector("#twidtter")
+const ig=document.querySelector("#instagram")
+const whts=document.querySelector("#whatsapp")
+const li=document.querySelector("#linkedin")
+const dc=document.querySelector("#discord")
+const rd=document.querySelector("#reddit")
+const msg=document.querySelector("#messenger")
+
+function init(){
+    let postUrl =encodeURI(document.location.href);
+    let postTitle = encodeURI("Hi everyone, please check this out: ");
+    // tw.href = `https://twitter.com/share?url=${postUrl}&text=${postTitle}&hashtags=javascript,programming`;
+    fb.setAttribute("href",
+    `https://www.facebook.com/sharer.php?u=${postUrl}`);
+    
+    whts.setAttribute("href",
+    `https://api.whatsapp.com/send?text=${postTitle} ${postUrl}`);
+    rd.setAttribute("href",
+    `https://reddit.com/submit?url=${postUrl}&title=${postTitle}`);
+    li.setAttribute("href",
+    `https://www.linkedin.com/sharing/share-offsite/?url=${postUrl}`);
+}
+init();
